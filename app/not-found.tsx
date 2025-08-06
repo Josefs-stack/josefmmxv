e722d931d0a426709465000895c5c418c23e28ea
josef_stack <cj.devs@outlook.com>
2025-08-06 07:23:16 -0300

    create not-found

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react';
+
+export default function NotFound() {
+
+  return (
+    <section className="ErroPage overflow-y-scroll scrollbar-none">
+      <div className='w-full h-full flex justify-center items-center'>
+        <h1 className='w-full h-[50%] flex justify-center items-center'>404</h1>
+      </div>
+    </section>
+  );
+}
